86d2101fba956792e1799abca43956ffa1df8a60
ststeiger <quandary82@hailmail.net>
2020-02-06 09:58:30 +0100

    Coordinates

--- TestSQLite/Program.cs
+++ TestSQLite/Program.cs
@@ -2,11 +2,19 @@
 namespace TestSQLite
 {
 
+
+#if true 
     using SQLiteConnection = Community.CsharpSqlite.SQLiteClient.SqliteConnection;
     using SQLiteCommand = Community.CsharpSqlite.SQLiteClient.SqliteCommand;
     using SQLiteDataReader = Community.CsharpSqlite.SQLiteClient.SqliteDataReader;
-    
-    
+#else
+    using SQLiteConnection = System.Data.SQLite.SQLiteConnection;
+    using SQLiteCommand = System.Data.SQLite.SQLiteCommand;
+    using SQLiteDataReader = System.Data.SQLite.SQLiteDataReader;
+#endif
+
+
+
     class Program
     {
         
@@ -20,8 +28,15 @@ namespace TestSQLite
                 string path = "/root/github/ststeiger/VectorTileServer/VectorTileServer/wwwroot/2017-07-03_france_monaco.mbtiles";                
                 
                 if(System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
-                    path = @"d:\username\wwwroot\2017-07-03_france_monaco.mbtiles";
-                
+                    path = @"file:///D:/username/Documents/Visual%20Studio%202017/Projects/VectorTileServer/VectorTileServer/wwwroot/2017-07-03_france_monaco.mbtiles";
+                    // path = @"file:///D:/username/Documents/Visual Studio 2017/Projects/VectorTileServer/VectorTileServer/wwwroot/2017-07-03_france_monaco.mbtiles";
+                    // path = @"D:/username/Documents/Visual Studio 2017/Projects/VectorTileServer/VectorTileServer/wwwroot/2017-07-03_france_monaco.mbtiles";
+                    // path = @"D:\username\Documents\Visual Studio 2017\Projects\VectorTileServer\VectorTileServer\wwwroot\2017-07-03_france_monaco.mbtiles";
+
+                //if ("COR".Equals(System.Environment.UserDomainName, System.StringComparison.InvariantCultureIgnoreCase))
+                // path = @"D:\username\Downloads\2017-07-03_planet_z0_z14.mbtiles";
+
+
                 using (SQLiteConnection conn = new SQLiteConnection(string.Format("Data Source={0};Version=3;", path)))
                 {
                     conn.Open();
@@ -49,17 +64,30 @@ namespace TestSQLite
             return stream;
         } // End Function GetRawTile 
 
-        
-        
-        
+
+
+
+        // https://alastaira.wordpress.com/2011/07/06/converting-tms-tile-coordinates-to-googlebingosm-tile-coordinates/
+        public static int FromTmsY(int tmsY, int zoom)
+        {
+            return (1 << zoom) - tmsY - 1; // = 2^zoom - tmsY - 1
+        } // End Function FromTmsY 
+
+
+
         static void Main(string[] args)
         {
             // https://localhost:44378/tiles/8528/5975/14
-            // curl 'https://localhost:44378/tiles/8528/5975/14
-            // System.IO.Stream strm = GetRawTile(8528, 5975, 14);
-            System.IO.Stream strm = GetRawTile(8528, 5975, 14);
+            int x = 8528, y = 5975, z = 14;
+            y = FromTmsY(y, z);
+
+            System.IO.Stream strm = GetRawTile(x, y, z);
             System.Console.WriteLine(strm.Length);
             System.Console.WriteLine(" --- Press any key to continue --- ");
         }
+
+
     }
+
+
 }
